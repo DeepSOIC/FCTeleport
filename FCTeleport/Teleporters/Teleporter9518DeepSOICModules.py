f728a20a8d85d89c4b0f18bb6e89714b6db1a69f
DeepSOIC <vv.titov@gmail.com>
2017-04-06 02:29:06 +0300

    Add DeepSOIC C++ module porting (temporary)
    converts C++ module containers to PartDesign Body. Maybe it's better to
    convert to part-o-magic Module, but it's much more involved.

--- FCTeleport/Teleporters/Teleporter9518DeepSOICModules.py
+++ FCTeleport/Teleporters/Teleporter9518DeepSOICModules.py
@@ -2,24 +2,21 @@ from .. import FCProject
 from .. import Teleport
 
 class ThisTeleport(Teleport.Teleport):
-    for_revision = 9518
+    for_revision = 9518 # artificial number, to be applied before porting PartDesign Bodies!
     brief_description = "Body ported to use extensions"
     def upgradeProject(self, project):
-        bb = project.findObjects("PartDesign::Body")
+        bb = project.findObjects("Part::BodyBase")
         for b in bb:
+            b.setTypeId('PartDesign::Body')
             try:
                 b.renameProperty('Model', 'Group')
             except Exception as err:
                 self.err("property 'Model' expected but not found")
         # extension can be omitted, FreeCAD seems to deal with it just fine
+        
 
     def downgradeProject(self, project):
-        bb = project.findObjects("PartDesign::Body")
-        for b in bb:
-            try:
-                b.renameProperty('Group', 'Model')
-            except Exception as err:
-                self.err("property 'Group' expected but not found")
-        # extension can be omitted, FreeCAD seems to deal with it just fine
+        pass
+        #(backwards conversion is impossible)
 
 Teleport.registry.registerTeleport(ThisTeleport)
--- FCTeleport/Teleporters/__init__.py
+++ FCTeleport/Teleporters/__init__.py
@@ -1,1 +1,2 @@
 from . import Teleporter9518BodyExtension
+from . import Teleporter9518DeepSOICModules
